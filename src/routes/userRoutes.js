fc9dd19ac819f89be21aa6534856cb23721a30d6
Matheus lima <matheusslimabm@gmail.com>
2024-09-04 20:05:22 -0300

    User create

---
+++ src/routes/userRoutes.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Router } from 'express';
+import userController from '../controllers/UserController';
+
+const router = new Router();
+
+router.post('/', userController.store);
+
+export default router;
