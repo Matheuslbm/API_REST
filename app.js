23d648db43da3961a2cc11e6617ca1534839af3a
Matheus lima <matheusslimabm@gmail.com>
2024-09-04 18:03:33 -0300

    conectei ao banco mariadb

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import homeRoutes from './src/routes/homeRoutes';
 
