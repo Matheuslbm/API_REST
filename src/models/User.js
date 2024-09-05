83dfef635468c3255addf4bfb9350802734b656c
Matheus lima <matheusslimabm@gmail.com>
2024-09-05 11:19:47 -0300

    CRUD

--- src/models/User.js
+++ src/models/User.js
@@ -45,7 +45,9 @@ export default class User extends Model {
     })
 
     this.addHook('beforeSave', async user => {
+     if (user.password) {
       user.password_hash = await bcryptjs.hash(user.password, 8);
+     }
     })
 
     return this;
