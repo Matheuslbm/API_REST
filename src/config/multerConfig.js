eff0831fde39a07bd7dda6f52d5143bca2e35907
Matheus lima <matheusslimabm@gmail.com>
2024-09-06 15:07:36 -0300

    Criando caminho para uploads

---
+++ src/config/multerConfig.js
@@ -0,0 +1,11 @@
+import multer from 'multer';
+import { extname, resolve } from 'path';
+
+export default {
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, resolve(__dirname, '..', '..', 'uploads'));
+    },
+    filename: (req, file, cb) => {},
+  })
+}
